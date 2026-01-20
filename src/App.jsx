193b8cc103b4cd9f4648e444b56132e79a85955d
Lakshmi Kumari V <lakshmokumarisingh8@gmail.com>
2026-01-20 14:00:30 +0530

    msg

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+export default function App(){
+  return(
+    <>
+    <h1>Hello World</h1>
+    <p>Welcome to React!</p>
+    <img src="https://reactjs.org/logo-og.png" alt="React Logo" width="400" height="200"/><br>
+    </br>
+    <br>
+    </br>
+    <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn more about React</a><br>
+    </br>
+    <a href="https://www.youtube.com/" target="_blank">youtube</a>
+    <br>
+    </br>
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ul>
+    <audio controls src ="https://pixabay.com/music/"></audio>
+    <video controls src="https://filesamples.com/samples/video/mp4/sample_640x360.mp4" type="video/mp4"></video>
+    <button>Click Me</button>
+    
+    </>
+  )
+  
+}
