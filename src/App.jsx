5034b72298beab3ceb3a82b81657d7770960ecfb
Lakshmi Kumari V <lakshmokumarisingh8@gmail.com>
2026-02-09 14:02:01 +0530

    day2

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
+import { useState } from "react"
+
 export default function App(){
 
-  const btn = [1,2,3,4,5,6,7,8,9,10];
-  const item = ["apple","mango","guava","litchii","banana","grapes","orange","papaya","kiwi","watermelon"];
+
+  const[name, setName] = useState("");
+  const[email,setEmail] = useState("");
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    console.log(name);
+    console.log(email);
+  }
   return(
     <>
-  <h1>Day6</h1>
-  {btn.map((button,index)=>(
-    <button key={index}>{button}</button>
-  ))}
-  <ul>
-    {item.map((fruits,index)=>(
-      <li key={index}>{fruits}</li>
-    ))}
-  </ul>
+    <h1>signup</h1>
+    <form>
+      <label>Name</label>
+      <div>
+        <input
+        type="text"
+        placeholder="enter your name"
+        value={name}
+        onChange={(e)=>setName(e.target.value)}
+        ></input>
+      </div>
+      <br></br>
+      <label>Email</label>
+      <div>
+        <input
+        type="email"
+        placeholder="enter your email"
+        value={email}
+        onChange={(e)=>setEmail(e.target.value)}
+        >
+        </input>
+      </div>
+      <br></br>
+      <button type="submit" onClick={handleSubmit}>Submit</button>
+    </form>
     </>
   )
 }
